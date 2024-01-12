7aa1a2d604b57d6d7b4d5c4acd03b45a21e13b52
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2024-01-12 21:28:18 +0545

    Database connected

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import express from 'express';
-const server = express();
+import cors from 'cors';
+import  bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
-server.get('/demo',(req,res) => {
+const DB = 'mongodb+srv://shreeshaaatreya1:IEbwYGMsEKUe811S@cluster0.ifiyo49.mongodb.net/Daan-Griha?retryWrites=true&w=majority';
+
+mongoose.connect(DB).then(() =>{
+    console.log('Connection successful');
+}).catch((err) => console.log('Connection failed'));
+
+
+const server = express();
+ 
+server.use(cors());
+server.use(bodyParser.json());
+
+server.post('/demo',(req,res) => {
+    console.log(req.body)
     res.send('hello');
 });
 
